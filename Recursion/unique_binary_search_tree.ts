cc307c39d91d00db0562e53dc9b641019ef41bce
ghun131 <hungdh131@gmail.com>
2024-07-09 20:05:39 +0700

    unique binary serach tree

--- Recursion/unique_binary_search_tree.ts
+++ Recursion/unique_binary_search_tree.ts
@@ -9,4 +9,43 @@ class TreeNode {
   }
 }
 
-function generateTrees(n: number): Array<TreeNode | null> {}
+function allPossibleBST(
+  start: number,
+  end: number,
+  memo: { [key: string]: Array<TreeNode | null> }
+): Array<TreeNode | null> {
+  const res: Array<TreeNode | null> = [];
+  if (start > end) {
+    res.push(null);
+    return res;
+  }
+  const key: string = start + "," + end;
+  if (memo[key] != undefined) {
+    return memo[key];
+  }
+  for (let i = start; i <= end; ++i) {
+    const leftSubTrees: Array<TreeNode | null> = allPossibleBST(
+      start,
+      i - 1,
+      memo
+    );
+    const rightSubTrees: Array<TreeNode | null> = allPossibleBST(
+      i + 1,
+      end,
+      memo
+    );
+    for (const left of leftSubTrees) {
+      for (const right of rightSubTrees) {
+        let root = new TreeNode(i, left, right);
+        res.push(root);
+      }
+    }
+  }
+  memo[key] = res;
+  return res;
+}
+function generateTrees(n: number): Array<TreeNode | null> {
+  const memo: { [key: string]: Array<TreeNode | null> } = {};
+  return allPossibleBST(1, n, memo);
+}
+
